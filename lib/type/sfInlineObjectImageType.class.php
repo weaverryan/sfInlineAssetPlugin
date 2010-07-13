69e1eaf5ae2165408910b089f78230a7de0f94dc
weaverryan <ryan@thatsquality.com>
2010-07-13 17:19:15 -0500

    Updating syntax for changes in inlineobjectplugin

--- lib/type/sfInlineObjectImageType.class.php
+++ lib/type/sfInlineObjectImageType.class.php
@@ -16,30 +16,29 @@ class sfInlineObjectImageType extends sfInlineObjectType
    * @see sfInlineObjectType
    * @return string
    */
-  public function render()
+  public function render($name, $arguments)
   {
     sfApplicationConfiguration::getActive()->loadHelpers(array('Asset', 'Tag', 'Url'));
 
-    $options = $this->getOptions();
-    $format = isset($options['format']) ? $options['format'] : null;
-    $link = isset($options['link']) ? $options['link'] : false;
+    $format = isset($arguments['format']) ? $arguments['format'] : null;
+    $link = isset($arguments['link']) ? $arguments['link'] : false;
     unset(
-      $options['format'],
-      $options['link']
+      $arguments['format'],
+      $arguments['link']
     );
 
-    $attrs = InlineObjectToolkit::arrayToAttributes($options);
+    $attrs = InlineObjectToolkit::arrayToAttributes($arguments);
 
     /**
      * @todo If sfImageTransformExtraPlugin is present, use the format option
      */
-    $img =  image_tag($this->getWebPath(), $attrs);
+    $img =  image_tag($this->getWebPath($name), $attrs);
 
     if ($link)
     {
       // render the image with a link around it - the link has a set of attributes
       $linkAttrs = sfConfig::get('app_inline_asset_link_attributes', array());
-      $linkAttrs['href'] = public_path($this->getWebPath());
+      $linkAttrs['href'] = public_path($this->getWebPath($name));
 
       return content_tag('a', $img, $linkAttrs);
     }
@@ -54,9 +53,9 @@ class sfInlineObjectImageType extends sfInlineObjectType
    *
    * @return string
    */
-  public function getFilenamePath()
+  public function getFilenamePath($name)
   {
-    return sfConfig::get('sf_uploads_dir').'/'.$this->getName();
+    return sfConfig::get('sf_uploads_dir').'/'.$name;
   }
 
   /**
@@ -64,8 +63,8 @@ class sfInlineObjectImageType extends sfInlineObjectType
    *
    * @return string
    */
-  public function getWebPath()
+  public function getWebPath($name)
   {
-    return '/uploads/'.$this->getName();
+    return '/uploads/'.$name;
   }
 }
